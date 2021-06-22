d46d77f961b6540db7d2754af4e8905310a671d2
Misako Kondoh <kondoh.misako@gmail.com>
2021-06-22 16:55:36 +0900

    crate business card

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+console.log(`
+Misako Kondoh
+Twitter JP: https://twitter.com/misas2pink
+Twitter EN: https://twitter.com/__misakd
+GitHub:     https://github.com/misa335
+Qiita:      https://qiita.com/__misakd
+LinkedIn:   https://www.linkedin.com/in/misa-k-engineer/
+
+Please contact me with one of channels above ;D`);
