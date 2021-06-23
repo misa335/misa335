2109c471c7351dc1e847f689b23d02f3053280c3
Misako Kondoh <kondoh.misako@gmail.com>
2021-06-23 09:53:42 +0900

    Update business card

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 console.log(`
-Misako Kondoh
-Twitter JP: https://twitter.com/misas2pink
-Twitter EN: https://twitter.com/__misakd
-GitHub:     https://github.com/misa335
-Qiita:      https://qiita.com/__misakd
-LinkedIn:   https://www.linkedin.com/in/misa-k-engineer/
+♥･*:.｡ ｡.:*･ﾟ♡･*:.｡ ｡.:*･ﾟ♡･*:.｡ ｡.:*･ﾟ♡･*:.｡ ｡.:*･ﾟ♡･*:.｡ ｡.:*･ﾟ♥
+    Misako Kondoh
 
-Please contact me with one of channels above ;D`);
+    GitHub:     https://github.com/misa335
+    Twitter JP: https://twitter.com/misas2pink
+    Twitter EN: https://twitter.com/__misakd
+    Qiita:      https://qiita.com/__misakd
+    LinkedIn:   https://www.linkedin.com/in/misa-k-engineer/
+
+    Please contact me with one of channels above ;D
+♥･*:.｡ ｡.:*･ﾟ♡･*:.｡ ｡.:*･ﾟ♡･*:.｡ ｡.:*･ﾟ♡･*:.｡ ｡.:*･ﾟ♡･*:.｡ ｡.:*･ﾟ♥`
+);
